c87eee6f546a97ff9977fdb5486c60aa435ef9e6
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-01-21 09:12:07 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Search />
       <Hosts />
       <Activities />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+  return (
+    <div className="flex flex-wrap h-auto p-10 leading-7 bg-gray-100">
+      <ul className="mr-64">
+        <li>
+          <h2 className="font-semibold mb-2">A propos</h2>
+        </li>
+        <li>
+          <a href="#">Fonctionnement du site</a>
+        </li>
+        <li>
+          <a href="#">Conditions générales de vente</a>
+        </li>
+        <li>
+          <a href="#">Données et confidentialité</a>
+        </li>
+      </ul>
+
+      <ul className="mr-64">
+        <li>
+          <h2 className="font-semibold mb-2">Nos hébergements</h2>
+        </li>
+        <li>
+          <a href="#">Charte qualité</a>
+        </li>
+        <li>
+          <a href="#">Soumettre votre hôtel</a>
+        </li>
+      </ul>
+
+      <ul className="mr-64">
+        <li>
+          <h2 className="font-semibold mb-2">Assistance</h2>
+        </li>
+        <li>
+          <a href="#">Centre d'aide</a>
+        </li>
+        <li>
+          <a href="#">Nous contacter</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
